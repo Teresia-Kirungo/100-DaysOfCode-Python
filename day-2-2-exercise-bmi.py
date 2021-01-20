ca23015012c1974c1d78ee3025e3626e1b34734a
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-20 18:37:58 +0300

    Update day-2-2-exercise-bmi.py

--- day-2-2-exercise-bmi.py
+++ day-2-2-exercise-bmi.py
@@ -2,9 +2,9 @@
 # Instructions
 # Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
 # The BMI is a measure of some's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
-# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m): weight /(height * height)
 
-**Warning** you should convert the result to a whole number. 
+# **Warning** you should convert the result to a whole number. 
 
 # Example Input: weight = 80, height = 1.75
 #   80 ÷ (1.75 x 1.75) =  26.122448979591837
