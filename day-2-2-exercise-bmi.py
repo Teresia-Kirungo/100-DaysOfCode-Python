915d8785874d6a847851f2a01e90fc0820e72df9
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-20 18:44:13 +0300

    Update day-2-2-exercise-bmi.py

--- day-2-2-exercise-bmi.py
+++ day-2-2-exercise-bmi.py
@@ -17,3 +17,26 @@
 # 3. Remember PEMDAS.
 # 4. Remember to convert your result to a whole number (int). 
 
+# SOURCE CODE 👇
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+print(type(height)) #checks the data type of the height which is a str
+print(type(weight)) #checks the data type of the weight which is a str
+
+# Conversion of the data types to intergers
+new_height = float(height)
+new_weight = int(weight)
+
+# Confirms if the data type conversion was successful
+print(type(new_height))
+print(type(new_weight))
+
+# Calculation of the bmi, the int() function converts the float to an interger(a whole number without a decimal)
+bmi = int(new_weight / (new_height * new_height))
+print(bmi)
+
