4876440b8086dbe2ad358e89da4ea14624b78616
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-21 14:52:33 +0300

    Update day-3-1-odd-or-even.py

--- day-3-1-odd-or-even.py
+++ day-3-1-odd-or-even.py
@@ -21,9 +21,9 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 if number % 2 == 0:
-  print("This is an even number")
+  print("This is an even number.")
 else:
-  print("This is an odd number")
+  print("This is an odd number.")
 
 
 
