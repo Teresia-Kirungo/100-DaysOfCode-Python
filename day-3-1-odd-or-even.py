86179910b50b6b0ffecf57a56ab6f066bdc7bbfb
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-21 14:38:17 +0300

    Update day-3-1-odd-or-even.py

--- day-3-1-odd-or-even.py
+++ day-3-1-odd-or-even.py
@@ -7,10 +7,24 @@
 # Odd Numbers
 # e.g. 59 is **odd** because 59 ÷ 2 = 29.5
 # 29.5 is not a whole number, it has decimal places. Therefore there is a remainder of 0.5, so the division is not clean.
+
 # The **modulo** is written as a percentage sign (%) in Python. It gives you the remainder after a division. 
-# e.g. 
-# 6 ÷ 2 = 3 with no remainder. 
+# e.g. 6 ÷ 2 = 3 with no remainder. 
 # 6 % 2 = 0
 
 # 5 ÷ 2 = 2.5 remainder is 1.
 # 5 % 2 = 1
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
+
+
+
+
