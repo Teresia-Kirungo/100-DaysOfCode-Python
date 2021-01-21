d9e432ad57bb51dae3f0385cc7078f19e6980a1e
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-21 10:02:01 +0300

    Update day-2-3-life-in-weeks.py

--- day-2-3-life-in-weeks.py
+++ day-2-3-life-in-weeks.py
@@ -15,7 +15,7 @@
 # Source Code
 
 # 🚨 Don't change the code below 👇
-age = input("What is your current age?")
+age = input("What is your current age? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
