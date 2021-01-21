cb1e820929f1886766e8678a78352709a6c6d35f
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-21 15:16:41 +0300

    Update and rename day-3-2-bmi2.py to day-3-2-Bmi-2.py

---
+++ day-3-2-Bmi-2.py
@@ -0,0 +1,20 @@
+# BMI Calculator 2.0
+# Instructions
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+# - Under 18.5 they are underweight
+# - Over 18.5 but below 25 they have a normal weight
+# - Over 25 but below 30 they are slightly overweight
+# - Over 30 but below 35 they are obese
+# - Above 35 they are clinically obese.
+
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+# Formula -> weight/ (height **2)
+
+
+# round() off the result to the nearest whole number. The interpretation message needs to include the words in bold from the interpretations above. e.g. underweight, normal weight,  overweight, obese, clinically obese. 
+
+# Hint
+# 1. Try to use the **exponent** operator in your code.
+# 2. Remember to **round** your result to the nearest whole number. 
+# 3. Make sure you include the words in **bold** from the interpretations. 
