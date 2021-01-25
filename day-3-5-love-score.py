5980a747bdceb2527e2031fd49de264e06819700
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-25 11:24:08 +0300

    Update day-3-5-love-score.py

--- day-3-5-love-score.py
+++ day-3-5-love-score.py
@@ -1,3 +1,35 @@
+# Love Calculator
+# Instructions
+# You are going to write a program that tests the compatibility between two people.  
+
+# To work out the love score between two people:
+
+# Take both people's names and check for the number of times the letters in the word TRUE occurs. Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers to make a 2 digit number. 
+
+# For Love Scores **less than 10** or **greater than 90**, the message should be: "Your score is **x**, you go together like coke and mentos."
+
+# For Love Scores **between 40** and **50**, the message should be: "Your score is **y**, you are alright together."
+
+# Otherwise, the message will just be their score. e.g.: "Your score is **z**."
+
+# Example input: name1 = "Angela Yu", name2 = "Jack Bauer"
+
+# T occurs 0 times, R occurs 1 time, U occurs 2 times, E occurs 2 times, 
+# Total = 5
+
+# L occurs 1 time, O occurs 0 times, V occurs 0 times, E occurs 2 times
+# Total = 3
+
+# Love Score = 53
+
+# Print: "Your score is 53."
+
+# Hint
+# 1. The `lower()` function changes all the letters in a string to lower case. 
+# 2. The `count()` function will give you the number of times a letter occurs in a string. 
+
+# View this Code LIVE here -> https://repl.it/@terrykirungo/day-3-5-love-score#main.py
+
 # 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
