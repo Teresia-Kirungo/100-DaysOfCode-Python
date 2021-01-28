938c20429561dbb0718fde95c5c4fb822f835d59
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-28 16:37:07 +0300

    Create day-4-5-nested.py

---
+++ Day 4/day-4-5-nested.py
@@ -0,0 +1,14 @@
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Apples", "Grapes", "Lettuce", "Cabbages", "Pears", "Peaches"]
+
+# Out of the dirty dozens, the fruits are:
+fruits = ["Strawberries", "Apples", "Grapes", "Pears", "Peaches"]
+
+# Out of the dirty dozens, the veggies are are:
+veggies = ["Spinach", "Kale","Lettuce", "Cabbages"]
+
+# A nested list looks like this
+new_sorted_dozen = [fruits, veggies]
+
+print(new_sorted_dozen)
+# This what it will look like
+# [['Strawberries', 'Apples', 'Grapes', 'Pears', 'Peaches'], ['Spinach', 'Kale', 'Lettuce', 'Cabbages']]
