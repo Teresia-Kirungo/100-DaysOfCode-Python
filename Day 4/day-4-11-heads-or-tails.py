937c4f3049d2da0265b1da9aaa9054972908728d
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-28 16:03:05 +0300

    Create day-4-11-heads-or-tails.py

---
+++ Day 4/day-4-11-heads-or-tails.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+# randomly picks a whole number(interger) between 0 and 1
+random_side = random.randint(0, 1)
+
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+# What arrays are in JavaScript is what Lists are in Python
+# Arr =[Item1, Item2, Item3]
+# List = [Item1, Item2, Item3]
+
+
+
+
+
+
+
+
+
