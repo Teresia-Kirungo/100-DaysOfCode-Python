602f80e0080ba1a472630edaca9f40202b70c7de
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-28 16:00:30 +0300

    Update day-4-1-randomisation.py

--- Day 4/day-4-1-randomisation.py
+++ Day 4/day-4-1-randomisation.py
@@ -4,6 +4,6 @@ import random # import the random module
 random_interger = random.randint(1, 5) # Generates random number between 1 and 5
 print(random_interger)
 
-# Random float starts from 0.0 to 0.9 it never gets to 1
+# Random float starts from 0.0 to 0.9 it never gets to 1, code on line 8 will give you a number between 1 to 5 but 5 won't be included.
 random_float = round(random.random() * 5) #round() rounds off to the nearest whole number.
 print(random_float)
