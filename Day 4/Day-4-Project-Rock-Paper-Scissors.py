21d71f7e05237568202d6e0176ccbf28a8c2958a
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-29 09:11:06 +0300

    Create Day-4-Project-Rock-Paper-Scissors.py

---
+++ Day 4/Day-4-Project-Rock-Paper-Scissors.py
@@ -0,0 +1,25 @@
+# Rock paper scissors
+# There are only two outcomes to this game a draw, or a win.
+
+# The rules of the game
+# 1. Rock beats Scissors
+# 2. Paper beats Rock
+# 3. Scissor beats Paper
+
+#  If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie. 
+# If you win you get 2 points, 
+#   If you lose 1 point, 
+#     If you lose 0 points.
+
+rock = ''' 
+    ROCK 
+'''
+
+paper = '''
+    PAPER
+'''
+
+scissors = '''
+    SCISSORS
+'''
+
