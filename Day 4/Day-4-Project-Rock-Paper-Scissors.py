12a7e0cef66989e8870e2f9b7e3e22012d6058b6
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-29 19:58:01 +0300

    Update Day-4-Project-Rock-Paper-Scissors.py

--- Day 4/Day-4-Project-Rock-Paper-Scissors.py
+++ Day 4/Day-4-Project-Rock-Paper-Scissors.py
@@ -28,33 +28,36 @@ print("Welcome to Rock, Paper, Scissors!")
 game = [rock, paper, scissors]
 
 users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
-print(f"Your choice is: \t{game[users_choice]}")
-
-computer_choice = random.randint(0, 2)
-print(f"Computer choice is: \t{game[computer_choice]}")
-
-# draw
-if users_choice == computer_choice:
-    print("Its a draw, you both get 1 point")
-
-# Paper Beats Rock
-elif users_choice  == 0 and computer_choice == 1:
-    print("Paper beats Rock. Computer Wins 2 points! You 0 points.")
-elif users_choice  == 1 and computer_choice == 0:
-    print("Paper beats Rock. You Win 2 points! Computer 0 points.")
-  
-# Rock beats Scissors
-elif users_choice  == 0 and computer_choice == 2:
-    print("Rock beats Scissors. You Win 2 points! Computer 0 points.")
-elif users_choice  == 2 and computer_choice == 0:
-    print("Rock beats Scissors. Computer Wins 2 points! You 0 points.")
-
-# Scissor beats Paper
-elif users_choice  == 1 and computer_choice == 2:
-    print("Scissor beats Paper. Computer Wins 2 points! You 0 points.")
-elif users_choice  == 2 and computer_choice == 1:
-    print("Scissor beats Paper. You Win 2 points! Computer 0 points.")
-
-elif users_choice  >= 3 or users_choice <0:
+# Checks for invalid numbers first
+if users_choice >= 3 or users_choice < 0:
     print("Invalid input try again!")
+else:
+    print(f"Your choice is: \t{game[users_choice]}")
 
+    computer_choice = random.randint(0, 2)
+    print(f"Computer choice is: \t{game[computer_choice]}")
+
+
+    # draw
+    if users_choice == computer_choice:
+        print("Its a draw, you both get 1 point")
+
+    # Paper Beats Rock
+    elif users_choice  == 0 and computer_choice == 1:
+        print("Paper beats Rock. Computer Wins 2 points! You 0 points.")
+    elif users_choice  == 1 and computer_choice == 0:
+        print("Paper beats Rock. You Win 2 points! Computer 0 points.")
+  
+    # Rock beats Scissors
+    elif users_choice  == 0 and computer_choice == 2:
+        print("Rock beats Scissors. You Win 2 points! Computer 0 points.")
+    elif users_choice  == 2 and computer_choice == 0:
+        print("Rock beats Scissors. Computer Wins 2 points! You 0 points.")
+
+    # Scissor beats Paper
+    elif users_choice  == 1 and computer_choice == 2:
+        print("Scissor beats Paper. Computer Wins 2 points! You 0 points.")
+    elif users_choice  == 2 and computer_choice == 1:
+        print("Scissor beats Paper. You Win 2 points! Computer 0 points.")
+    else:
+        print("Invalid input once again!")
