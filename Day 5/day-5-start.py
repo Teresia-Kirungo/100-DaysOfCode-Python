77c8f0c50c9947792ee55dd456af883718f8a569
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-02-08 10:41:16 +0300

    Update day-5-start.py

--- Day 5/day-5-start.py
+++ Day 5/day-5-start.py
@@ -1,5 +1,5 @@
 # Loops -> Things that have to happen over and over again, # Password generator that uses letters, symnols and numbers
-fruits = ["Apples", "Peaches", "Pear"]
+fruits = ["Apples", "Peach", "Pear"]
 
 # for loops allow you to iterate in a list
 for fruit in fruits:
