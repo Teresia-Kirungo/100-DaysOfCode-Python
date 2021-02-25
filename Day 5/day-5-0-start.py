ad84f8d7cabc1fffbbdf7e3d2df24d2917ac8f6b
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-02-25 15:01:52 +0300

    Update day-5-0-start.py

--- Day 5/day-5-0-start.py
+++ Day 5/day-5-0-start.py
@@ -1,10 +1,10 @@
-# Loops -> Things that have to happen over and over again, # Password generator that uses letters, symnols and numbers
-fruits = ["Apples", "Peach", "Pear"]
+# Loops -> Things that have to happen over and over again
+fruits = ["Apple", "Peach", "Pear"]
 
-# for loops allow you to iterate in a list
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie") # loops through the list and adds pie
-    
-# Everytime the for loop runs the variable fruit is assigned to an item in the fruits list
+for tunda in fruits:
+    print(tunda)
+    print("Here's an " + tunda + " pie") # loops through the list and adds pie
+# Everytime the for loop runs the variable tunda is assigned to an item in the fruits list i.e
+#   Apple
+#   Here's an Apple pie
 # It excutes until the last item in the List
