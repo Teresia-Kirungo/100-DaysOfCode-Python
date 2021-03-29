466e0789f19f997a2d1d70f2717c439e91feaaaa
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-03-29 15:45:58 +0300

    Update day-5-1-average-height.py

--- Day 5/day-5-1-average-height.py
+++ Day 5/day-5-1-average-height.py
@@ -19,12 +19,12 @@
 # 1. Remember to use the `round()` function to round the average height before you print it.
 # Test Your Code
 
-# # Solution
+# Solution
 
-# https://repl.it/@terrykirungo/day-5-1-average-height
+# https://replit.com/@terrykirungo/day-5-1-average-height
 
-# 🚨 Don't change the code below 👇
-# split() puts the items separated by a comma(,) in a List like this 123, 145, 236, 267, 190#
+# Don't change the code below
+# split() puts the items separated by a comma(,) in a List like this 123, 145, 236, 267, 190
 # 123 145 236 267 190
  
 student_heights = input("Input a list of student heights ").split()
@@ -34,22 +34,22 @@ print(student_heights)
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 
-# 🚨 Don't change the code above 👆
+# Don't change the code above 
 
-#Write your code below this row 👇
+# Write your code below this row 👇
 
 # First assigns the total_height variable to 0
 total_height = 0 
 # Uses the for loop below to calculate the total sum of the student_heights
 for height in student_heights:
-    total_height = total_height + height # the first one it will execute 0 + the first number in the list provided, it will run this line of code until the last item on the list
-print(f"The sum of the students height is {total_height}") # prints total height calculated!
+    total_height = total_height + height # the first one it will execute 0 which is what we have assigned to the total height + the first item(height number) in the list provided, it will run this line of code until the last item on the list
+print(f"The total sum of the students height is {total_height}") # prints total height calculated!
 
 # First assigns the no_of_students variable to 0
 number_of_students = 0
 for student in student_heights:
-  number_of_students += 1
-print(f"The number of students is {number_of_students}")
+  number_of_students += 1 #Does number_of_students + 1 everytime it counts student(item) in student_heights
+print(f"The number of students is participating is {number_of_students}")
 
 average_height = round(total_height / number_of_students)
 print(f"The average height is {average_height}")
