25a20b5978e481a899d825fdb6177e96cc7cbbbb
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-03-29 15:46:36 +0300

    Update day-5-1-average-height.py

--- Day 5/day-5-1-average-height.py
+++ Day 5/day-5-1-average-height.py
@@ -1,5 +1,5 @@
 # Average Height
-#  Instructions
+# Instructions
 # You are going to write a program that calculates the average student height from a List of heights. 
 # e.g. `student_heights = [180, 124, 165, 173, 189, 169, 146]`
 
