9ab688e359b944a5295d430c9ce57c473b129610
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-03-29 17:55:29 +0300

    Update day-5-4-fizz-buzz.py

--- Day 5/day-5-4-fizz-buzz.py
+++ Day 5/day-5-4-fizz-buzz.py
@@ -21,4 +21,15 @@
 # Solution
 # https://replit.com/@terrykirungo/day-5-4-fizz-buzz
 
+#Write your code below this row 👇
 
+for number in range(1,101): #prints from 1 to 100
+    if number % 3 == 0 and number % 5 == 0: #have it at the top so that it proceeds with the execution
+        number = "FizzBuzz"
+    elif number % 3 == 0:
+        number = "Fizz"
+    elif number % 5 == 0:
+        number = "Buzz"
+    print(number)
+    
+# If we have this if number % 3 == 0 and number % 5 == 0: at the bottom the program willl execute to completion before it reaches this block of code rememeber the rock paper scissor game!
