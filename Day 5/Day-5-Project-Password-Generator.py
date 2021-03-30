56f44ade17a9101388a73cde5d541a62b2f627e4
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-03-30 20:01:26 +0300

    Update Day-5-Project-Password-Generator.py

--- Day 5/Day-5-Project-Password-Generator.py
+++ Day 5/Day-5-Project-Password-Generator.py
@@ -10,9 +10,24 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the Terri-PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
+random_letter = random.randint(0,nr_letters-1)
+letter_picked = letters.pop(random_letter)
+print()
+
+random_number = random.randint(0,nr_numbers-1)
+number_picked = numbers.pop(random_number)
+print()
+
+random_symbols = random.randint(0,nr_symbols-1)
+symbols_picked = symbols.pop(random_symbols)
+print(letter_picked + symbols_picked + number_picked)
+
+for item in
+# password = nr_letters + nr_symbols + nr_numbers
+# print(password)
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
