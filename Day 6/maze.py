3dd9263f45798bd648b8624bf66d2262300b8a8f
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-05 17:33:39 +0300

    Update maze.py

--- Day 6/maze.py
+++ Day 6/maze.py
@@ -1,3 +1,5 @@
+# This is the solution to the Reeborg Maze python game
+# Find it here http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 def turn_right():
     turn_left()
     turn_left()
