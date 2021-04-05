c3e6784ad5f57de706fea50b90427e9f8029dd17
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-05 17:28:47 +0300

    Create maze.py

---
+++ Day 6/maze.py
@@ -0,0 +1,25 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while at_goal -= True!:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
+
+
+
+
+
+
+
+
+
+
+
