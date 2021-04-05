6cd87f36a660a99524be71312b12b71bef8c6c3d
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-05 18:08:20 +0300

    Update start.py

--- Day 7/start.py
+++ Day 7/start.py
@@ -1,1 +1,7 @@
-
+# Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses.
+# How to play Hangman Game: 
+#   The player guessing the word may, at any time, attempt to guess the whole word. If the word is correct, the game is over and the guesser wins. 
+#   Otherwise, the other player may choose to penalize the guesser by adding an element to the diagram. 
+#   On the other hand, if the other player makes enough incorrect guesses to allow his opponent to complete the diagram, the game is also over, this time with the guesser losing. 
+#   However, the guesser can also win by guessing all the letters that appear in the word, thereby completing the word, before the diagram is completed.
+# Read more about it here: https://en.wikipedia.org/wiki/Hangman_(game) | Play it here: https://hangmanwordgame.com/
