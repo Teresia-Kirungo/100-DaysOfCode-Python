4852bf25d1c75c2d07f44650a56901392ea3937d
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-05 19:08:23 +0300

    Create day-7-hangman-1.py

---
+++ Day 7/day-7-hangman-1.py
@@ -0,0 +1,21 @@
+#Step 1 imports the random module
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = random.choice(word_list)
+
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+answer = input("Hey! Guess a letter in the chosen word ")
+guess = answer.lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word
+for letter in chosen_word:
+    if letter == guess:
+        print("It's a match")
+    else:
+        print("Try again!")
