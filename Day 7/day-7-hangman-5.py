e9bc24bf89a2eb037ffab03335dcc717b39b11fd
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-06 11:53:48 +0300

    Update day-7-hangman-5.py

--- Day 7/day-7-hangman-5.py
+++ Day 7/day-7-hangman-5.py
@@ -27,17 +27,19 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    if guess in display:
+        print(f"You've already guessed {guess}")
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     #Check if user is wrong.
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life!")
         lives -= 1
         if lives == 0:
             end_of_game = True
