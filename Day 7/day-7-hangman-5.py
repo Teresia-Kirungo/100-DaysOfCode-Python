26a49ac95037a30ca8e8677c5bc75251a0c473d2
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-06 11:46:37 +0300

    Update day-7-hangman-5.py

--- Day 7/day-7-hangman-5.py
+++ Day 7/day-7-hangman-5.py
@@ -12,7 +12,8 @@ end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-from hangman_art import stages
+from hangman_art import logo
+print(logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -51,4 +52,6 @@ while not end_of_game:
         print("You win.")
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
+
     print(stages[lives])
