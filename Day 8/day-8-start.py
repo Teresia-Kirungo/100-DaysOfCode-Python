d63523dc15b51c75df4c11a4cd030ae7c29cb946
Replit user <replituser@example.com>
2021-04-21 05:35:47 +0000

    Created day 8 start file

---
+++ Day 8/day-8-start.py
@@ -0,0 +1,10 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+    print("Hi")
+    print("My name is Terry.")
+    print("I am a flutter developer.")
+
+greet()
