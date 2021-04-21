27c95b3958157c1b31bd85bf6bc420987cd0d18b
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-21 10:16:13 +0300

    Update day-8-2-prime-number-checker.py

--- Day 8/day-8-2-prime-number-checker.py
+++ Day 8/day-8-2-prime-number-checker.py
@@ -2,7 +2,7 @@
 # Instructions
 # Prime numbers are numbers that can only be cleanly divided by itself and 1. 
 
-# **You need to write a function** that checks whether if the number passed into it is a prime number or not.
+# You need to write a function that checks whether if the number passed into it is a prime number or not.
 # e.g. 2 is a prime number because it's only divisible by 1 and 2.
 # But 4 is not a prime number because you can divide it by 1, 2 or 4.
 
