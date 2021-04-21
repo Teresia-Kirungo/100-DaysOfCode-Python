d101306c38d60bf25b1996a939d4bb2c5ec4f931
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-21 10:37:44 +0300

    Update day-8-2-prime-number-checker.py

--- Day 8/day-8-2-prime-number-checker.py
+++ Day 8/day-8-2-prime-number-checker.py
@@ -20,3 +20,18 @@
 # Test Your Code
 # https://replit.com/@terrykirungo/day-8-2-prime-number-checker#main.py
 
+#Write your code below this line 👇
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number-1):
+        if number % i == 0:
+            is_prime = False
+    if is_prime == True:
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} not a prime number.")    
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
