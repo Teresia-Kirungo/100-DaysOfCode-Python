765abe656fe84aeafa36043f51cf696ad1deb132
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-22 20:42:38 +0300

    Update and rename 1.py to day-9-dictionaries.py

---
+++ Day 9/day-9-dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionaries in python allow us to group items together in the text beneath
+# "Bug" is the key anf the text "An error..." is the value
+# "Function" is the key and the text "A piece of code.." is the value
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again",
+    }
+
+
