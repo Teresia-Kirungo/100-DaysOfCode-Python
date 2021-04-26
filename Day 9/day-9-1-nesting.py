2d1b316a14b48d730d6c9cfa98ef7ca394bb84b9
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-26 20:58:55 +0300

    Create day-9-1-nesting.py

---
+++ Day 9/day-9-1-nesting.py
@@ -0,0 +1,30 @@
+
+#Nesting Dictionary in a Dictionary
+
+travel_log = {
+  "France": {
+      "cities_visited": ["Paris", "Lille", "Dijon"], # a list nested inside a dictionary
+      "total_visits": 12
+      },
+  "Germany": {
+      "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+       "total_visits": 5
+       },
+}
+
+#Nesting Dictionary in a List
+
+travel_log = [
+  {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"], 
+    "total_visits": 12
+  },
+  {
+    "country": "Germany", 
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+    "total_visits": 5
+  },
+]
+
+
