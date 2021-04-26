e71665bb3076eca72ea3998dfde1601aab10bed4
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-04-26 20:41:40 +0300

    Update day-9-1-grading-program.py

--- Day 9/day-9-1-grading-program.py
+++ Day 9/day-9-1-grading-program.py
@@ -27,3 +27,30 @@
 # Test Your Code
 # https://replit.com/@terrykirungo/day-9-1-exercise#main.py
 
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+for score in student_scores:
+    grade = student_scores[score]
+    if grade > 90 :
+        student_grades[score]= "Outstanding"
+    elif grade > 80:
+        student_grades[score]= "Exceeds Expectations"
+    elif grade > 70:
+        student_grades[score]= "Acceptable"
+    else:
+        student_grades[score]= "Fail"    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
