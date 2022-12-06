9810e254297e89cc7c3bc24019a96b5131b17b16
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-07 00:31:13 +0300

    Built a bmi calculator.

---
+++ Day 2/4-bmi-calculator.py
@@ -0,0 +1,27 @@
+# BMI Calculator
+# Instructions: https://replit.com/@terrykirungo/day-2-4-bmi-calculator
+
+# 🚨 Don't change the code below 👇
+height = input("Enter your height in Meters(m): ")
+weight = input("Enter your weight in Kilograms(kg): ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+print(type(height), height) #checks the data type of the height which is a str
+print(type(weight), weight) #checks the data type of the weight which is a str
+
+# Conversion of the data types to intergers
+# 👇opted to use the float data types incase a user uses a whole number or one with a decimal point 👇
+new_height = float(height) 
+new_weight = float(weight)
+
+# Confirms if the data type conversion was successful
+print(type(new_height))
+print(type(new_weight))
+
+# Calculation of the bmi, the int() function converts the float to an interger(a whole number without a decimal)
+bmi = int(new_weight / (new_height * new_height))
+# bmi = int(new_weight / (new_height ** 2)) is another formula, **2 means raised to the power of two
+print(f"Your BMI is {bmi}.")
+
+#Test it here: https://replit.com/@terrykirungo/day-2-4-bmi-calculator#main.py
