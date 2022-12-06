d8e7c306400c8e46f3c45b4028335d2e6d1f051d
@terry_kirungo <40103113+Teresia-Kirungo@users.noreply.github.com>
2022-12-06 23:15:04 +0300

    Introduction to hello world and the input method
    Printing to console
    Using the input method.
    Use of concatenation
    Use of tabs

---
+++ Day1/0-printing-start-1.py
@@ -0,0 +1,18 @@
+# Write your code below this line - this is a comment
+
+# print() function, displays what you want it to display/print.
+print("Hello world!") #our first hello world in Python
+
+#\t is for tabs
+print("\t My name is Terry, welcome to my Python Playbook!")
+
+# + means Concatenation, in this case it's joining two strings
+print("Book" + " Mark!") 
+
+# input() function when we need feedback from the user
+input("What is your name? ")
+
+# what is your name will be printed first, then the whole script 
+print('Hello! ' + input("What is your friend's name? "))
+
+#Test it here: https://replit.com/@terrykirungo/0-1-printing-start-1#main.py
