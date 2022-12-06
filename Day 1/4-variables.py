2fc7dd76010d71a982731fdcbc84df696876475e
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-06 23:45:47 +0300

    Use of variables in temporary storage of data

---
+++ Day 1/4-variables.py
@@ -0,0 +1,27 @@
+# Variables
+# Instructions: https://replit.com/@terrykirungo/4-variables
+
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a # creates a new variable c that holds the value(contents) of a
+a = b # asigns a to be the value of b
+b = c # asigns b to be the value of what a was which is now held in c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+# Test it here: 👇
+# https://replit.com/@terrykirungo/4-variables#main.py
