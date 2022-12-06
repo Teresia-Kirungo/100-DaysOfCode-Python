c18b4c4bbf9dbdf242172b58d78fa9d61fd7ab00
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-06 23:33:54 +0300

    Debugged

---
+++ Day 1/2-debugging.py
@@ -0,0 +1,17 @@
+# Debugging
+# Instructions: https://replit.com/@terrykirungo/2-debugging#README.md
+
+# New lines can be created with a backslash and n.
+
+#Fix the code below  -> the code is fixed! 😁👍
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+#Line 26 can also be written like this 👇
+# print("String Concatenation is done with the \"+\" sign.")
+# use of the forward slash to tell the computer the "" in between are also to be printed
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Test The code here:👇
+# https://replit.com/@terrykirungo/2-debugging#main.py
