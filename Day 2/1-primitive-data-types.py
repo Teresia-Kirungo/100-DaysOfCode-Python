4ce61afd06e75ff439a7406df4e139f8855d5950
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-07 00:22:07 +0300

    Primitive data type and data type conversion.

---
+++ Day 2/1-primitive-data-types.py
@@ -0,0 +1,21 @@
+# Data Types
+# Subcripting, pulling out the first index of a string, e.g in print("Hello[0]") will print out the first letter in Hello which is H, the character in the index 0 
+
+#STRING
+print("'Hello[0]' prints the first letter of the word Hello " + "Hello"[0])
+print("'Hello[-1]' prints the last letter of the word Hello " + "Hello"[-1])
+
+#Interger
+print(123+345)
+
+#Float, a number with a decimals point
+print(float(3.6775))
+
+#Boolean
+# True False
+
+a = float(123)
+b = str(a) #str converts into string, this line converts the float in a to a string
+print(a)
+
+# Test it here: https://replit.com/@terrykirungo/1-primitive-data-types#main.py
---
+++ Day 2/2-data-type-conversion.py
@@ -0,0 +1,8 @@
+num_char = len(input("What is your name? "))
+print(f"Your name is an {type(num_char)}") # num_char is an int data type
+
+new_num_char = str(num_char) #converts num_char to a string
+
+print(f"Your name has " + new_num_char + " characters.")
+
+#Test it here: https://replit.com/@terrykirungo/2-data-type-conversion#main.py
