38e417ed919833eeaaa60ef02fd06b2f5d4100e8
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-07 03:09:57 +0300

    Bug fixed

--- Day 6/maze.py
+++ Day 6/maze.py
@@ -1,18 +1,18 @@
 # This is the solution to the Reeborg Maze python game
 # Find it here http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
     
-while at_goal -= True!:
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
+# while at_goal -= True!:
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
         
 
 
