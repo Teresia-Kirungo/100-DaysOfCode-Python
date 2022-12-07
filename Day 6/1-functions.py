5e0636383b92782ab515322d3fcea19738138966
Teresia-Kirungo <terrykirungo@gmail.com>
2022-12-07 03:08:35 +0300

    Learnt about functions and we can use them in the maze game

---
+++ Day 6/1-functions.py
@@ -0,0 +1,19 @@
+# This is an example of a print function
+print("Hello world!")
+
+# When we want to start our own functions we start with a keyword def as shown below
+#def stands for defining
+# after defining our function we give it a name e.g
+def name_of_the_function():
+    print("This is how you create a funtion: using the keyword def and the name of the function")
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# This is how you call the function
+my_function()
+
+#You call the function outside the scope of that function in simple terms outside it's indentation
+name_of_the_function()
+
+# Test it here: https://replit.com/@terrykirungo/1-functions
