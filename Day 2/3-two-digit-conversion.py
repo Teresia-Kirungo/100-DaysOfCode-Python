ac8e36b5e1bc60a1ecfc9871adef9cb473f6ac7d
@terry_kirungo <terrykirungo@gmail.com>
2023-01-03 14:25:35 +0300

    sum of two digit number

--- Day 2/3-two-digit-conversion.py
+++ Day 2/3-two-digit-conversion.py
@@ -9,21 +9,21 @@ two_digit_number = input("Type a two digit number: ")
 #Write your code below this line 👇
 
 # find out the data type of the input received
-print(type(two_digit_number), two_digit_number) # the input received from the user is a string
+print(two_digit_number, " is of type ",type(two_digit_number)) # the input received from the user is a string
 
 # Converting the string data type to intergers
 first_digit = int(two_digit_number[0])
-print(type(first_digit), first_digit)
+print(first_digit, "after type conversion is of type",type(first_digit))
 
 last_digit = int(two_digit_number[1])
-print(type(last_digit), last_digit)
+print(last_digit, "after type conversion is of type",type(last_digit))
 
-# Find the data type, checks if conversion was successful
+# Checks if conversion was successful
 # print(type(first_digit)) # successful conversion of string data type to interger
 # print(type(last_digit))
 
 # Prints the sum of the two-digit-number
 sum = first_digit + last_digit
-print(f"The sum of the two-digit-number is: {sum}")
+print("The sum of the two-digit-number is:", sum)
 
 # Test it here: https://replit.com/@terrykirungo/3-two-digit-conversion#main.py
