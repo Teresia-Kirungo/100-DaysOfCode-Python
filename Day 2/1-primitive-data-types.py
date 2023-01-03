e3a17c363c1c1d0c80c2a403500aaa181d2d909b
@terry_kirungo <terrykirungo@gmail.com>
2023-01-03 12:48:42 +0300

    Updated lecture notes. did type conversion and debugged type error

--- Day 2/1-primitive-data-types.py
+++ Day 2/1-primitive-data-types.py
@@ -2,13 +2,20 @@
 # Subcripting, pulling out the first index of a string, e.g in print("Hello[0]") will print out the first letter in Hello which is H, the character in the index 0 
 
 #STRING
-print("'Hello[0]' prints the first letter of the word Hello " + "Hello"[0])
-print("'Hello[-1]' prints the last letter of the word Hello " + "Hello"[-1])
+print("'Hello[0]' prints the first letter of the word " + "Hello"[0])
+print("'World[-1]' prints the last letter of the word " + "World"[-1])
+
+
+print("Terry"[0]) #prints the first letter of the word T
+
+#String -> because of the curly braces around the intergers
+print("123," + "345")
 
 #Interger
 print(123+345)
 
 #Float, a number with a decimals point
+print("This is a float 👇 ")
 print(float(3.6775))
 
 #Boolean
