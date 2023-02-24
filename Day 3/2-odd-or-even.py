09ec9548a2038b50131612a88650827e17f0b4c4
Terry <terrykirungo@gmail.com>
2023-02-24 10:06:20 +0300

    testing for odd and even numbers using modulo

---
+++ Day 3/2-odd-or-even.py
@@ -0,0 +1,15 @@
+# Odd or Even
+# Instructions: https://replit.com/@terrykirungo/2-odd-or-even#README.md
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+# We know that dividing using % modulo gives you a reminder, so any number % 2 that gives a reminder(a number) is an odd number
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
+
+# Test it here: https://replit.com/@terrykirungo/2-odd-or-even#main.py
